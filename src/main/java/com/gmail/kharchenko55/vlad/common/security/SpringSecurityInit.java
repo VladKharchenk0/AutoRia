1bccdf33fc0dc839b6b05d3ce6b942a1a7af38c5
VladKharchenk0 <vlad.kharchenko55@gmail.com>
2020-05-31 15:20:51 +0300

    Added Spring Security.

---
+++ src/main/java/com/gmail/kharchenko55/vlad/common/security/SpringSecurityInit.java
@@ -0,0 +1,8 @@
+package com.gmail.kharchenko55.vlad.common.security;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SpringSecurityInit extends AbstractSecurityWebApplicationInitializer {
+}
---
+++ src/test/java/com/gmail/kharchenko55/vlad/AutoRiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gmail.kharchenko55.vlad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoRiaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
