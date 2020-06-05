7766ae4a06dd09cc15db5728b15f27dda97a2f3e
VladKharchenk0 <vlad.kharchenko55@gmail.com>
2020-06-05 20:00:06 +0300

    Added log out logic

--- src/main/java/com/gmail/kharchenko55/vlad/controller/MainController.java
+++ src/main/java/com/gmail/kharchenko55/vlad/controller/MainController.java
@@ -1,19 +1,29 @@
 package com.gmail.kharchenko55.vlad.controller;
 
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.security.Principal;
+
 @Controller
 public class MainController {
 
-    @GetMapping("/")
-    public String root() {
+    @GetMapping({"/", "/index"})
+    public String root(Principal principal, Model model) {
+        model.addAttribute("email", principal.getName());
         return "index";
     }
 
     @GetMapping("/login")
     public String login(Model model) {
+        return "login";
+    }
+
+    @GetMapping("/log_out")
+    public String logOut() {
+        SecurityContextHolder.getContext().setAuthentication(null);
         return "login";
     }
 
