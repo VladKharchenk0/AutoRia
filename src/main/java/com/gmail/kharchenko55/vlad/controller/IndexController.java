d1aa2348ff2742a419df51938ae6bc5b754c5011
VladKharchenk0 <vlad.kharchenko55@gmail.com>
2020-06-07 12:44:44 +0300

    Added OkHttp and created first Get request

--- src/main/java/com/gmail/kharchenko55/vlad/controller/IndexController.java
+++ src/main/java/com/gmail/kharchenko55/vlad/controller/IndexController.java
@@ -1,12 +1,30 @@
 package com.gmail.kharchenko55.vlad.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
+import com.gmail.kharchenko55.vlad.common.Util;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
+@RestController
 public class IndexController {
-    @RequestMapping({"index", "index.html"})
-    public String index() {
-        return "index";
+
+    private OkHttpClient client = new OkHttpClient();
+    private String baseUrl = Util.getBaseUrl();
+
+    @GetMapping("/search")
+    public String index() throws IOException {
+        Request request = new Request.Builder()
+                .url(baseUrl+ "category_id=1")
+                .get()
+                .build();
+
+        Response response = client.newCall(request).execute();
+
+        return response.body().string();
     }
 }
