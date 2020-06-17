27cc999c586ea6d44f2e12d8c1b06adf5bebfa0b
VladKharchenk0 <vlad.kharchenko55@gmail.com>
2020-06-17 12:18:04 +0300

    Added JsonCar.java

---
+++ src/main/java/com/gmail/kharchenko55/vlad/controller/searchcontroller/JsonCar.java
@@ -0,0 +1,9 @@
+package com.gmail.kharchenko55.vlad.controller.searchcontroller;
+
+import lombok.Data;
+
+public @Data class JsonCar {
+    private int carBody;
+    private int carBrand;
+    private int carModel;
+}
